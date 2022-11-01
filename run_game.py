3972aff736be9c8fe40f30fa5ae3e71208ec8b75
lishiqianhugh <lishiqian2001@gmail.com>
2022-11-01 10:24:25 +0800

    fix some bugs and add fill game

--- run_game.py
+++ run_game.py
@@ -14,11 +14,16 @@ import pdb
 class IPHYRE():
     def __init__(self):
         self.game, self.mode = sys.argv[1], sys.argv[2]
-        self.HEIGHT, self.WIDTH = 1000, 1000
+        self.HEIGHT, self.WIDTH = 600, 600
         self.FPS = 60
         self.timestep = 1 / self.FPS
         self.max_time = 10
-        self.num_ball = len(game_paras[self.game]['ball'])
+
+        self.blocks = game_paras[self.game]['block']
+        self.eli = game_paras[self.game]['eli']
+        self.dynamic = game_paras[self.game]['dynamic']
+        self.balls = game_paras[self.game]['ball']
+        self.num_ball = len(self.balls)
         self.b_mass, self.b_elasticity, self.b_friction = 1.0, 0.1, 0.5
         self.l_friction, self.l_elasticity = 0.5, 0.1
         self.G = (0., 100.0)
@@ -31,7 +36,8 @@ class IPHYRE():
             'hinder': [[(450., 320.), 1], [(500., 320.), 1.5]],
             'direction': [[(150., 180.), 1], [(100., 350.), 1.5]],
             'hole': [[(250., 100.), 1], [(250., 150.), 2.]],
-            'multi_balls': [[(500., 400.), 1]]
+            'multi_balls': [[(500., 400.), 1]],
+            'fill': []
         }
 
         if self.mode != 'collect':
@@ -81,16 +87,16 @@ class IPHYRE():
         self.space.add(body, shape)
 
     def add_all(self):
-        assert len(game_paras[self.game]['block']) == len(game_paras[self.game]['eli'])
-        for l_para, eli, dynamics in zip(game_paras[self.game]['block'], game_paras[self.game]['eli'], game_paras[self.game]['dynamic']):
+        assert len(self.blocks) == len(self.eli)
+        for l_para, eli, dynamics in zip(self.blocks, self.eli, self.dynamic):
             if dynamics:
                 self.add_dynamic_line(l_para, self.l_friction, self.l_elasticity)
             else:
                 self.add_static_line(l_para, eli, self.l_friction, self.l_elasticity)
-        for b_para in game_paras[self.game]['ball']:
+        for b_para in self.balls:
             self.add_ball(b_para[:2], b_para[2], self.b_mass, self.b_elasticity, self.b_friction)
 
-    def eliminate(self, p, eli):
+    def eliminate(self, p):
         for i, body in enumerate(self.space.bodies[:-self.num_ball]):
             shape = list(body.shapes)[0]
             x, y = body.position
@@ -98,8 +104,9 @@ class IPHYRE():
             max_0 = x + 10
             min_1 = y - 10
             max_1 = y + 10
-            if eli[i] == 1 and min_0 < p[0] < max_0 and min_1 < p[1] < max_1:
+            if self.eli[i] == 1 and min_0 < p[0] < max_0 and min_1 < p[1] < max_1:
                 self.space.remove(shape, shape.body)
+                self.eli.pop(i)
                 return True
         return False
 
@@ -129,7 +136,7 @@ class IPHYRE():
                     sys.exit(0)
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     p = event.pos
-                    self.eliminate(p, game_paras[self.game]['eli'])
+                    self.eliminate(p)
 
             self.space.step(self.timestep)
             self.space.debug_draw(self.draw_options)
@@ -148,7 +155,7 @@ class IPHYRE():
                 p, t = action[step][0], action[step][1]
                 if time_count >= t:
                     # p = space.bodies[a].position
-                    if self.eliminate(p, game_paras[self.game]['eli']):
+                    if self.eliminate(p):
                         print(f'Step {step}: Click {p} at time {time_count}.')
                         step += 1
 
@@ -179,14 +186,14 @@ class IPHYRE():
                 p, t = action[step][0], action[step][1]
                 if time_count >= t:
                     # p = space.bodies[a].position
-                    if self.eliminate(p, game_paras[self.game]['eli']):
+                    if self.eliminate(p):
                         print(f'Step {step}: Click {p} at time {time_count}.')
                         step += 1
                         for body in self.space.bodies:
                             print(body.position)
 
             self.space.step(self.timestep)
-            if(interval_cal == interval or interval_cal ==0):
+            if interval_cal == interval or interval_cal ==0:
                 interval_cal = 0
                 for body in self.space.bodies:
                     positions.append(body.position)
@@ -202,15 +209,12 @@ class IPHYRE():
 
             interval_cal += 1
             time_count += self.timestep
-            
-        
-    
+
             if self.examine_success():
                 print(f'###### Success at time {time_count} ######')
                 for body in self.space.bodies:
                     print(body.position)
                 sys.exit()
-       
 
     def run(self, action=None):
         if action is None:
