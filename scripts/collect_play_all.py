ff4496dacdac652d5bf8e6ba9c10c9eb19aec7ec
lishiqianhugh <lishiqian2001@gmail.com>
2023-01-05 13:39:21 +0800

    update human study and its instructions

--- scripts/collect_play_all.py
+++ scripts/collect_play_all.py
@@ -1,4 +1,5 @@
 import sys
 from iphyre.utils import collect_play_all
 # player_name = sys.argv[1]
-collect_play_all('lsq', max_episode=1, save_path='../data/player_data.json')
+player_name = 'your_name'
+collect_play_all(player_name, max_episode=1, save_path=f'../data/{player_name}.json')
